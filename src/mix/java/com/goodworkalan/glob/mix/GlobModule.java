bbf8bde58c3c2f8577cbf0e1ee12c628f38fbf91
Alan Gutierrez <alan@blogometer.com>
2009-10-10 01:08:11 -0500

    Updated Mix build.
    - Changed version numbers.
    - Added Mix Eclipse.

--- src/mix/java/com/goodworkalan/glob/mix/GlobModule.java
+++ src/mix/java/com/goodworkalan/glob/mix/GlobModule.java
@@ -5,8 +5,8 @@ import com.goodworkalan.mix.BasicJavaModule;
 
 public class GlobModule extends BasicJavaModule {
     public GlobModule() {
-        super(new Artifact("com.goodworkalan", "glob", "0.1-SNAPSHOT"));
-        addDependency(new Artifact("com.goodworkalan", "cassandra", "0.7-SNAPSHOT"));
+        super(new Artifact("com.goodworkalan", "glob", "0.1"));
+        addDependency(new Artifact("com.goodworkalan", "cassandra", "0.7"));
         addTestDependency(new Artifact("org.testng", "testng", "5.10"));
         addTestDependency(new Artifact("org.mockito", "mockito-core", "1.6"));
     }
